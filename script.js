56a51d14e2924f89f9507e55368a3be498c8b180
Gabriel Bunhak <bunhakgabriel@gmail.com>
2023-10-17 05:11:30 -0300

    segundo commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let cont = 1
+let margin_left = -8.3
+let img1 = document.querySelector('.img1')
+
+let init = setInterval( () =>{
+
+    let margin;
+    img1.style.marginLeft = '0%'            
+
+    if(cont > 0){
+        margin = (cont * margin_left - 0.08 * cont).toFixed(1)
+        margin = margin + '%'
+        img1.style.marginLeft = margin   
+    }
+
+    cont++
+    if(cont == 10){cont = 0}            
+
+},40000000)
