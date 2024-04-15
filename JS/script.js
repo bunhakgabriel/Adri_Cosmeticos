8cc222248d639acbfe554f22139df60a58ed6d3d
Gabriel Bunhak <bunhakgabriel@gmail.com>
2024-04-15 06:43:42 -0300

    Inclusao de produtos

--- JS/script.js
+++ JS/script.js
@@ -55,6 +55,38 @@ for(let c = 0; c < produtos.length; c++){
     })
 }
 
+/* Função tipewriter */
 
+const titulo = [
+    'A','t','r','a','n','s','f','o','r','m',
+    'a','ç','ã','o','q','u','e','v','o','c',
+    'ê','d','e','s','e','j','a','c','o','m',
+    'e','ç','a','a','q','u','i','.'
+]
+
+const typeWriterTitle = document.getElementById('typeWriterTitle')
+let c = 0
+
+const typewriterInit = () => {
+
+    typeWriterTitle.innerText += titulo[c]
+
+    if(c === 0 || c === 13 || c === 16 || c === 20 || c === 26 || c === 32){
+        typeWriterTitle.innerHTML += `&nbsp;`
+    }
+
+    if(c === 13 || c === 32){
+        typeWriterTitle.innerHTML += `<br>` 
+    }
+
+    c++
+    if(c >= 38){
+        clearInterval(interval)
+    }
+}
+
+const interval = setInterval(typewriterInit, 100)
+
+/* Função scroll lateral imagens */
 
 
